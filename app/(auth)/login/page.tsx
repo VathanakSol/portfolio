a13216950351cf63f9c075f9ab5238ef5a89367b
Vathanak <vathanaksol1605@gmail.com>
2024-10-14 21:51:31 +0700

    last update

--- app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import Login from "@/components/Login";
-
-const SignInPage = () => (
-  <div className="flex justify-center items-center h-screen w-full">
-    <Login />
-  </div>
-)
-
-export default SignInPage;
--- app/(auth)/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import Signup from "@/components/Signup";
-
-const SignUpPage = () => (
-  <div className="flex justify-center items-center h-screen w-full">
-    <Signup />
-  </div>
-)
-
-export default SignUpPage;
