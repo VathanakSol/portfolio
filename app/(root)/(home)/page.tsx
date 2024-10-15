5474e1a41183d834444ee23f01e7d235c9f5470c
Vathanak <vathanaksol1605@gmail.com>
2024-10-15 16:46:46 +0700

    fix

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,7 @@
-
 import BackToTopButton from '@/components/BackToTopButton';
 import Header from '@/components/Header';
 import Projects from '@/components/Project';
 import ScrollProgressBar from '@/components/ScrollProgressBar';
-
-
 
 export default function HomePage() {
   return (
--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,13 +1,11 @@
-// import About from '@/components/About'
+
 import Timeline from '@/components/Timeline'
 import React from 'react'
 
 export default function page() {
   return (
     <main>
-        <Timeline />
-        {/* <About /> */}
-        
+        <Timeline />    
     </main>
   )
 }
