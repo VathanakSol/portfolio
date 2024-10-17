043264ec7670447e5a1bd35022da377f1cdf48eb
Vathanak <vathanaksol1605@gmail.com>
2024-10-17 13:46:06 +0700

    update code latest

--- components/Create-Repo.tsx
+++ components/Create-Repo.tsx
@@ -11,7 +11,7 @@ const CreateRepo = () => {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        const response = await fetch('https://naktech.pro/api/github', {
+        const response = await fetch('/api/github', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
