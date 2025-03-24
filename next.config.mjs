5544cd6041be49c2166659aa9e565935acfb9b36
VathanakSol <vathanaksol1605@gmail.com>
2025-03-24 23:50:13 +0700

    config in next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,23 @@ const nextConfig = {
     KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
     KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
   },
+  async headers() {
+    return [
+      {
+        source: "/ads.txt",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/plain",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
