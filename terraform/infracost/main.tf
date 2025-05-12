9e778c77e1e10323584e21b39dd951eb4a7785d5
Vathanak Sol <vathanaksol1605@gmail.com>
2025-05-12 11:12:05 +0700

    add new tf file

---
+++ terraform/infracost/main.tf
@@ -0,0 +1,16 @@
+provider "vercel" {
+  api_token = var.vercel_token  # Recommended to use variables
+}
+
+resource "vercel_project" "web_app" {
+  name      = "production-web-app"
+  framework = "nextjs"
+}
+
+resource "vercel_deployment" "main" {
+  project_id  = vercel_project.web_app.id
+  production  = true
+  files       = {
+    "pages/index.js" = file("${path.module}/pages/index.js")
+  }
+}
